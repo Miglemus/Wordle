40e7b6bc110766a1c6cbc30258cbc50157bc2d50
Miguel Lemus <miguel.lemus.1@ulaval.ca>
2026-06-25 23:11:30 -0400

    Increase the number of solutions tested in the benchmark and improve game reset logic

--- core/Benchmark.py
+++ core/Benchmark.py
@@ -10,21 +10,25 @@ class Benchmark:
         self._solver = solver
         self._statistics = {}
 
-    def run(self, num_solutions_tested: int = 200):
-        self._game.reset()
-        self._solver.reset()
+    def run(self, num_solutions_tested: int = 100000):
+        solutions_to_test = random.sample(list(get_all_solutions()), k=max(1, min(num_solutions_tested, len(get_all_solutions()))))
 
-        solutions_to_test = random.sample(list(get_all_solutions()), k=num_solutions_tested)
         for solution in solutions_to_test:
+            self._game.reset()
+            self._solver.reset()
             self._game.set_solution(solution)
+
             answer = None
             num_guesses = 0
-            while not self._game.is_over:
+            while not self._game.is_over and num_guesses < 5:
                 guess = self._solver.guess(answer)
                 answer = self._game.step(guess)
                 num_guesses += 1
 
             self._statistics[solution] = num_guesses
+            print(f"Solution: {solution}, Guesses: {num_guesses}")
+            
+        self.print_statistics()
 
     def print_statistics(self):
         total_guesses = sum(self._statistics.values())
